2473373e00c4ceba0f3e98f2231b6f1408cf7fbd
DAVID HO <davidhorph@hotmail.com>
2022-09-07 11:42:30 -0500

    styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './ShoppingList/shopping-list/shopping-list.component';
+import { RecipeListComponent } from './RecipeBook/recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent
+    ShoppingListComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule
