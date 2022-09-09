62981804d512334b657083207313cda7cbb7507b
DAVID HO <davidhorph@hotmail.com>
2022-09-09 10:59:44 -0500

    will redo according to Academind.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './ShoppingList/shopping-list/shopping-list.component';
-import { RecipeListComponent } from './RecipeBook/recipe-list/recipe-list.component';
-import { ShoppingListEditComponent } from './ShoppingList/shopping-list-edit/shopping-list-edit.component';
-import { RecipeItemComponent } from './RecipeBook/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './RecipeBook/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    ShoppingListComponent,
-    RecipeListComponent,
-    ShoppingListEditComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
