f3828c3bf26d995425d0cd85237ce911cdf8f04c
DAVID HO <davidhorph@hotmail.com>
2022-09-10 20:31:14 -0500

    recipe-detail.component.html.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,12 +7,19 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Monster Burger', 'Recipe for Monster Burger', 'https://www.happyfoodstube.com/wp-content/uploads/2020/10/halloween-monster-burgers-recipe.jpg')
+    new Recipe('Monster Burger', 'Recipe for Monster Burger', 'https://www.happyfoodstube.com/wp-content/uploads/2020/10/halloween-monster-burgers-recipe.jpg'),
+    new Recipe('Salmon Pasta', 'Delicious Salmon Pasta!!!', 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-smoked-salmon-pasta-horizontal-1542310073.png?resize=480:*'),
+    new Recipe('Cucumber Salad', 'Cucumber Salad Meal.', 'https://images-gmi-pmc.edge-generalmills.com/f920cb90-3541-49fb-b9e6-2f9add86302b.jpg'),
+    new Recipe('Iced Tea.', 'Chilled Iced Tea!!!', 'https://www.ohhowcivilized.com/wp-content/uploads/2019/05/0519-iced-tea-8-1.jpg'),
+    new Recipe('Ice cold water.', 'Cold, distilled water.', 'https://www.news-medical.net/image.axd?picture=2021%2F7%2Fshutterstock_1548860402.jpg')
   ];
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+
   }
 
 }
--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -3,6 +3,10 @@ export class Recipe {
   public description: string;
   public imagePath: string;
 
+  logArguments() {
+    return { name: this.name, description: this.description, imagePath: this.imagePath };
+  }
+
   constructor(name: string, desc: string, imagePath: string) {
     this.name = name;
     this.description = desc;
