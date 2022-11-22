41c5b4b9c92d76d3192e5032f4034d0cb39dda51
DAVID HO <davidhorph@hotmail.com>
2022-11-21 20:16:57 -0600

    Inside recipe-detail.component.html: replaced the Ingredients placeholder with an *ngfor that returns eacn new Ingredient( ) that is passed from the selectedRecipe: Recipe (inside recipes.component.ts) to recipe: recipe (recipe-detail.component.ts) via the @Input( ) decorator (parent => child passing data).

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -9,7 +9,7 @@ import { RecipeService } from '../Services/recipe.service';
   providers: [ RecipeService ] //registering a service.
 })
 export class RecipesComponent implements OnInit {
-  selectedRecipe: Recipe;
+  selectedRecipe: Recipe; //This will receive the details of whatever recipe was clicked via .subscribe() pass this onto the child recipe-detail via @Input() binding.
 
   constructor(private recipeService: RecipeService) { }
 
@@ -17,7 +17,7 @@ export class RecipesComponent implements OnInit {
     this.recipeService.recipeSelected.subscribe((recipe: Recipe) => {
       console.log({ selectedRecipe: this.selectedRecipe, recipe })
       return this.selectedRecipe = recipe
-    })
+    }) //Because we used .subscribe( ), whatever is emitted from recipeSelected (the name of the EventEmitter< >( ) we gave back in RecipeService class in recipe.service.ts), the argument recipe: Recipe will be whatever recipe was emitted when we click on one of the recipes from the list located on the LEFT of the app.
   }
 
 }
